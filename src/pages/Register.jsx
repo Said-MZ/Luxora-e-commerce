1258a6fda91f114d30c8f27e11d5e38149bccda1
Said zain <saidzain897@gmail.com>
2024-02-11 21:08:56 +0300

    Complete Register form UI

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Form, Link } from "react-router-dom";
+import { FormInput, SubmitBtn } from "../components";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center font-bold text-3xl">Register</h4>
+        <FormInput name="username" type="text" label="Username" />
+        <FormInput name="email" type="email" label="Email" />
+        <FormInput name="password" type="password" label="Password" />
+        <div className="mt-4">
+          <SubmitBtn text="Register" />
+        </div>
+        <p className="text-center">
+          Already a member?{" "}
+          <Link
+            className="link link-primary link-hover capitalize ml-2"
+            to={"/login"}
+          >
+            Login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 
 export default Register;
