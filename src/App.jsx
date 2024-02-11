7b97e25e9a0334143fc52281bccac1e5076a7747
Said zain <saidzain897@gmail.com>
2024-02-11 20:12:51 +0300

    Add routes and Error page UI

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,37 @@ import {
   Register,
   SingleProduct,
 } from "./pages";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "about", element: <About /> },
+      { path: "cart", element: <Cart /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "products", element: <Products /> },
+      { path: "products/:id", element: <SingleProduct /> },
+      { path: "orders", element: <Orders /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
 
 const App = () => {
-  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
-  return <div>HomeLayout</div>;
+  return (
+    <>
+      <nav>
+        <span className="text-4xl text-primary">Loxura</span>
+      </nav>
+      <Outlet />
+    </>
+  );
 };
 
 export default HomeLayout;
