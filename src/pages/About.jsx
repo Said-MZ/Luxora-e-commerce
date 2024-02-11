25a108b8e12aaac20602473a27611b278306d237
Said zain <saidzain897@gmail.com>
2024-02-12 01:41:23 +0300

    Add About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="text-secondary-content">
+      <div className="bg-base-content absolute shadow-2xl blur-[8rem] shadow-base-content -z-10 rounded-full opacity-25 w-[30rem] h-[30rem] top-0 -right-96 "></div>
+      <div className="bg-base-content absolute shadow-2xl blur-[8rem] shadow-base-content -z-10 rounded-full opacity-25 w-[30rem] h-[30rem] -bottom-2 -left-96 "></div>
+      <div className="bg-base-content absolute shadow-2xl blur-[10rem] shadow-base-content -z-10 rounded-full opacity-25 w-96 h-96 -bottom-[38rem] -right-80 md:-bottom-72 md:right-52 "></div>
+      <h1 className="text-3xl font-bold mb-5 text-primary">
+        About{" "}
+        <span
+          //glow text
+          className="px-1 text-base-content glow-gold"
+        >
+          Luxora{" "}
+        </span>
+        Store
+      </h1>
+      <div className="divider mb-8"></div>
+      <p className="mb-5">
+        Luxora Store is a modern e-commerce store that sells a variety of
+        products. We are dedicated to providing our customers with the best
+        products and services. We are committed to providing the best customer
+        service and making sure our customers are satisfied with their purchase.
+      </p>
+      <p>
+        We are a small business that is dedicated to providing our customers
+        with the best products and services. We are committed to providing the
+        best customer service and making sure our customers are satisfied with
+        their purchase.
+      </p>
+      <h2 className="text-2xl font-bold mt-28 mb-5 text-primary">
+        Our Mission
+      </h2>
+      <div className="divider mb-8"></div>
+      <p className="mb-5">
+        Our mission is to provide our customers with the best products and
+        services. We are committed to providing the best customer service and
+        making sure our customers are satisfied with their purchase.
+      </p>
+      <p>
+        We are dedicated to providing our customers with the best products and
+        services. We are committed to providing the best customer service and
+        making sure our customers are satisfied with their purchase.
+      </p>
 
-export default About
+      <h2 className="text-2xl font-bold mt-28 mb-5 text-primary">Our Vision</h2>
+      <div className="divider mb-8"></div>
+      <p className="mb-5">
+        Our vision is to be the best e-commerce store in the world. We are
+        committed to providing the best customer service and making sure our
+        customers are satisfied with their purchase.
+      </p>
+      <p>
+        We are dedicated to providing our customers with the best products and
+        services. We are committed to providing the best customer service and
+        making sure our customers are satisfied with their purchase.
+      </p>
+    </div>
+  );
+};
+
+export default About;
