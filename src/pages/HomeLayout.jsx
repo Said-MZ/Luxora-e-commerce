bb57beccdccb14b1a5212b2cf0d00d2819998515
Said zain <saidzain897@gmail.com>
2024-02-12 19:17:49 +0300

    Add loading

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Header, Navbar } from "../components";
+import Loading from "../components/Loading";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
       <Header />
       <Navbar />
       <section className="align-elements py-20">
-        <Outlet />
+        {isPageLoading ? <Loading /> : <Outlet />}
       </section>
     </>
   );
