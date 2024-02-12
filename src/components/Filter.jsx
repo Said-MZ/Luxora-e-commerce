62d5b1759eec55975038a4f2d8ae65fa19a2d70b
Said zain <saidzain897@gmail.com>
2024-02-12 18:21:31 +0300

    Add filter form, search input, and buttons

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Form, Link } from "react-router-dom";
+import FormInput from "./FormInput";
 
 const Filter = () => {
   return (
-    <div>Filter</div>
-  )
-}
+    <Form className="bg-bas-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <FormInput
+        label="search product"
+        name="search"
+        type="search"
+        size="input-sm"
+      />
+      <button type="submit" className="btn btn-primary btn-sm">
+        Search
+      </button>
+      <Link className="btn btn-neutral btn-sm" to={"/products"}>
+        Reset
+      </Link>
+    </Form>
+  );
+};
 
-export default Filter
+export default Filter;
