60d5f2f5bf6e43d3fba7689eb5f49cdc404fc534
Said zain <saidzain897@gmail.com>
2024-02-12 20:45:20 +0300

    Add pagination

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+  const pages = Array.from({ length: pageCount }, (_, i) => i + 1);
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    console.log(searchParams.toString());
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  if (pageCount < 2) {
+    return null;
+  }
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="btn btn-sm sm:btn-md join-item"
+          onClick={() => {
+            handlePageChange(1);
+          }}
+        >
+          ❮
+        </button>
+        {pages.map((pageNumber) => (
+          <button
+            key={pageNumber}
+            className={`btn btn-sm sm:btn-md join-item ${
+              pageNumber === page ? "btn-neutral" : ""
+            }`}
+            onClick={() => {
+              handlePageChange(pageNumber);
+            }}
+          >
+            {pageNumber}
+          </button>
+        ))}
+        <button
+          className="btn btn-sm sm:btn-md join-item "
+          onClick={() => {
+            handlePageChange(1);
+          }}
+        >
+          ❯
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PaginationContainer;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,15 +5,15 @@ import ProductsContainer from "../components/ProductsContainer";
 import PaginationContainer from "../components/PaginationContainer";
 
 const url = "/products";
-
 export const loader = async ({ request }) => {
   const params = Object.fromEntries([
-    ...new URLSearchParams(request.url).entries(),
+    ...new URL(request.url).searchParams.entries(),
   ]);
-
   const response = await customFetch(url, { params });
+
   const products = response.data.data;
   const meta = response.data.meta;
+
   return { products, meta, params };
 };
 const Products = () => {
